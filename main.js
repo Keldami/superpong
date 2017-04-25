f194dabc7c14ad7929022e633ac33b2564dd2e13
Artur Wa <art.wasinger@gmail.com>
2017-04-25 17:52:19 +0200

    superpong: little modifications

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert('Hallo Welt');
+var canvas = document.createElement('canvas');
+var width = 800;
+var height = 400;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+windows.onload = function() {
+    document.body.appendChild(canvas);
+    animate(step);
+};
+
+var step = function() {
+    render();
+    animate(step);
+};
+
+var render = function() {
+    context.fillStyle = "#FFFFFF";
+    context.fillRect(0, 0, width, height);
+};
