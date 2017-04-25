cdf19b04891f79cb8c1464c3ac0276e1e5fbcdd1
Artur Wa <art.wasinger@gmail.com>
2017-04-25 17:52:03 +0200

    some stuff happend

---
+++ pong.js
@@ -0,0 +1,23 @@
+Pong = {
+
+    //width, height and other settings of the game
+    Defaults: {
+        width:      640,
+        height:     480,
+        
+    },
+    
+    //colors of the given items
+    Colors: {
+    
+    }
+
+
+}
+
+//-------
+
+initialize: function(runner, cfg) {
+
+}
+
